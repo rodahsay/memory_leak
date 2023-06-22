38f8f664a37d51a6f4f2f470f81130e0f827cea4
Rodolfo Concepcion <rodolfo@AM003D.ahsayman.local>
2023-06-22 09:13:18 +0800

    Added variables to other components

--- src/app/components/component24/component24.component.ts
+++ src/app/components/component24/component24.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./component24.component.scss'],
 })
 export class Component24Component implements OnInit {
-
+  test1: string | undefined;
+  test2: string | undefined;
+  test3: string | undefined;
+  test4: string | undefined;
+  test5: string | undefined = "123";
+  test6: string | undefined = "123";
+  test7: string | undefined = "123";
+  test8: string | undefined = "123";
+  test9: string | undefined = "123";
+  test10: string | undefined = "123";
   constructor() { }
 
   ngOnInit() {}
